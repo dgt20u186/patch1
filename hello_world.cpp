71bb63ce69ce26aec356a319a8dcdbaf0c66cb2c
dgt20u186 <78912265+dgt20u186@users.noreply.github.com>
2021-03-09 11:58:42 +0300

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  string name:
+  std:cout << "Input your name: ";
+  std:cin >> name;
+  std:cout << "Hello world from " << name;
+  return 0;
+}
